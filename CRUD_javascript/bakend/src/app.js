45cdd4774e5c5014102a2f38d03eea76652fa982
Gabriel2113 <gasebas14@gmail.com>
2023-09-17 11:33:34 -0500

    prueba con postman para lectura de email y password

---
+++ CRUD_javascript/bakend/src/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// html
+
+app.get('/:name/:id', (req, res) => {
+    const{ name ,id } = req.params
+
+    res.send(`${name}: ${id}`);
+});
+
+app.post('/', (req, res)=>{
+    const {email, password} = req.body;
+    res.send(`${email}: ${password}`);
+});
+
+app.post('/', (req, res)=>{
+    res.send('Peticion PUT');
+});
+
+app.post('/', (req, res)=>{
+    res.send('Peticion DELETE');
+});
+
+app.listen(3000, ()=>{
+    console.log('Servidor encendido');
+});
