3c4015cf5a4f2d2d4f9918fd77d547ea59aaf235
Gabriel2113 <gasebas14@gmail.com>
2023-09-17 12:56:53 -0500

    creacion de querys desde JS

--- CRUD_javascript/bakend/src/app.js
+++ CRUD_javascript/bakend/src/app.js
@@ -37,20 +37,37 @@ const connection = myql2.createConnection({
     database: 'oasis',
  
  });
-
+ 
  connection.connect((err) => { 
-    if(err) throw err;
-    console.log ('BD conectada');
-});
-
-const querySql = 'SHOW TABLES;';
-
-connection.query(querySql, (err, res) =>{
-    if (err) throw err;
-    console.log('respuesta sql', res );
-});
-
-
-app.listen(3000, ()=>{
-    console.log('Servidor encendido');
-});
+     if(err) throw err;
+     console.log ('BD conectada');
+ });
+ 
+ const querySql = 'SHOW TABLES;';
+ 
+ connection.query(querySql, (err, res) =>{
+     if (err) throw err;
+     console.log('respuesta sql', res );
+ });
+ 
+ 
+ const insertQuery = `INSERT INTO compras_id(name, direccion, telefono, cantidad) VALUES('Santiago','Cra 123 c bis #45 a 21','318775407',4);`;
+ 
+ 
+ connection.query(insertQuery, (err, res)=> {
+     if (err) throw err;
+     console.log('respuesta insert', res)
+ })
+ 
+ const getQuery = `SELECT * FROM compras_id;`;
+ 
+ connection.query(getQuery, (err, res)=> {
+     if (err) throw err;
+     console.log('respuesta insert', res)
+ })
+ 
+ 
+ app.listen(3000, () => {
+     console.log('servidor encendido');
+ });
+ 
