41d7ea862fd00c5bf2ef60328e9b757b57d89430
Gaince-Yeison10 <jeigon555@gmail.com>
2023-11-14 10:55:59 -0500

    conexion variable base de datos produccion

--- Proyecto Oasis/Backend/src/config/database.js
+++ Proyecto Oasis/Backend/src/config/database.js
@@ -1,10 +1,10 @@
 const myql2 = require('mysql2');
 
 const  database = myql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'thoor',
-    database: 'Oasis'
+    host: 'bq6l1gq9lw8ifbajboj4-mysql.services.clever-cloud.com',
+    user: 'u16zvrkxtdcupipv',
+    password: 'Z0WaqEfnpGuhJP3xWzMR',
+    database: 'bq6l1gq9lw8ifbajboj4'
 });
 
 module.exports = database;
